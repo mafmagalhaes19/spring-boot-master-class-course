c57e9097b3a02a352e7c3d8557cf784531c5de48
mafmagalhaes19 <mafi.magalhaes@gmail.com>
2022-09-29 14:20:20 +0100

    Finished chapter 3 - dependency injection

--- demo/src/main/java/com/example/demo/customer/CustomerService.java
+++ demo/src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,5 +1,24 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+//Customer Service is a singleton
+@Component
 public class CustomerService {
+
+    private final CustomerRepo customerRepo;
+
+    @Autowired
+    public CustomerService(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+    List<Customer> getCustomer() {
+        return customerRepo.getCustomers();
+    }
 
 }
